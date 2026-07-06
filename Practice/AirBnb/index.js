d60db667740482ea9314d2d8d6fce56ce46e85e2
hemaksh3011 <hemakshkumawat1@gmail.com>
2026-07-06 17:04:07 +0530

    airbnb crud

--- Practice/AirBnb/index.js
+++ Practice/AirBnb/index.js
@@ -2,37 +2,76 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const listing = require("./models/listing.js");
+const path = require("path");
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.urlencoded({ extended: true }));
 
 async function main() {
-    await mongoose.connect("mongodb://127.0.0.1:27017/BIDES")
+  await mongoose.connect("mongodb://127.0.0.1:27017/BIDES");
 }
 
-main().then(()=>{
+main()
+  .then(() => {
     console.log("connected to db");
-}).catch((err)=>{
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
 
-app.get("/",(req,res)=>{
-    res.send("Here we go !!");
-})
+// app.get("/", (req, res) => {
+//   res.send("Here we go !!");
+// });
 
-app.get("/testListings",async (req,res)=>{
-    let sampleListing = new listing({
-        title:"home",
-        description:"beach side",
-        price:1200,
-        location:"goa",
-        country:"india",
-    });
-    await sampleListing.save();
-    console.log("Saved");
-    res.send("success");
+//Main route
+app.get("/", async (req, res) => {
+  const alllisting = await listing.find({});
+  res.render("listings/alllistings.ejs", { alllisting });
 });
 
-app.listen(3300,()=>{
-    console.log("Lets Get Started !!");
-})
+//add route
+app.get("/add-new", (req, res) => {
+  res.render("listings/add.ejs");
+});
+
+//show route
+app.get("/:id", async (req, res) => {
+  let { id } = req.params;
+  const lists = await listing.findById(id);
+  res.render("listings/show.ejs", { lists });
+});
+
+//posting new prop
+app.post("/", async (req, res) => {
+  //let { title, description, imageurl, price, location, country } = req.body;
+  let newlist = req.body.newlist;
+  const newlisting = new listing(newlist);
+  await newlisting.save();
+  res.redirect("/");
+});
+
+//edit property
+app.get("/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const edit = await listing.findById(id);
+});
+
+// app.get("/testListings", async (req, res) => {
+//   let sampleListing = new listing({
+//     title: "home",
+//     description: "beach side",
+//     price: 1200,
+//     location: "goa",
+//     country: "india",
+//   });
+//   await sampleListing.save();
+//   console.log("Saved");
+//   res.send("success");
+// });
+
+app.listen(3300, () => {
+  console.log("Lets Get Started !!");
+});
